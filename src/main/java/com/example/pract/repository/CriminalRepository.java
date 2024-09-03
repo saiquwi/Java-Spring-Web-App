2e4166c5436074dd9292293a9ddbce27d6195225
saiquwi <152636863+saiquwi@users.noreply.github.com>
2024-09-03 09:07:00 +0300

    Delete src directory

--- src/main/java/com/example/pract/repository/CriminalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pract.repository;
-
-import com.example.pract.entity.Criminal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CriminalRepository extends JpaRepository<Criminal, Long> {
-}
--- src/main/java/com/example/pract/repository/MissingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pract.repository;
-
-import com.example.pract.entity.Missing;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissingRepository extends JpaRepository<Missing, Long> {
-
-}
