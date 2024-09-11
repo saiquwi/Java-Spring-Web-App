5a01da7ce3598f06028923bfc34ff159c9c51103
saiquwi <152636863+saiquwi@users.noreply.github.com>
2024-09-11 14:04:43 +0300

    Delete src directory

--- src/main/java/com/example/pract/enums/CriminalStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.pract.enums;
-
-public enum CriminalStatus {
-    WANTED,
-    ARRESTED,
-    DEAD,
-    NO_TRACE;
-}
--- src/main/resources/static/js/home.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let img = document.querySelector('img');
-    img.classList.add('fade-in');
-});
